9fe76995111e130710cc2b4ba0bda9723fe2f564
IJFOOTE <isaiahfoote985@gmail.com>
2025-10-29 16:53:50 -0400

    Normalize character class input to lowercase

--- project1_starter.py
+++ project1_starter.py
@@ -47,6 +47,7 @@ def calculate_stats(character_class, level):
 
 
 def create_character(name, character_class, level=1):
+    character_class = character_class.lower()    
     classes = ['warrior', 'cleric', 'mage', 'rogue']
     if character_class not in classes:
         print('Invalid character class: ' + character_class)
